0dc73fabacb74ed6ba9e7c0fcb12d00279d4e2a8
Kamil Braun <kb346840@students.mimuw.edu.pl>
2019-12-15 15:27:34 +0100

    introduce Send (call without reponse)

--- Task.hs
+++ Task.hs
@@ -41,33 +41,40 @@ emptyQ :: Queue a
 emptyQ = Queue { front = [], back = [] }
 
 newtype Task mod (d :: Nat) a = Task
-    { runTask :: IO (Either (Call mod d a) a) }
+    { runTask :: IO (TaskValue mod d a) }
 
-data Call mod (d :: Nat) a where
-    Call :: (Module mod', d' + 1 <= d) => Task mod' d' b -> (b -> Task mod d a) -> Call mod d a
+data TaskValue mod (d :: Nat) a where
+    Pure :: a -> TaskValue mod d a
+    Call :: (Module mod', d' + 1 <= d) => Task mod' d' b -> (b -> Task mod d a) -> TaskValue mod d a
+    Send :: Module mod' => Task mod' d' () -> Task mod d a -> TaskValue mod d a
 
 instance Functor (Task mod d) where
     fmap f (Task m) = Task $ flip fmap m $ \case
-        Left (Call remote k) -> Left  $ Call remote $ fmap f . k
-        Right v           -> Right $ f v
+        Pure v -> Pure $ f v
+        Call remote k -> Call remote $ fmap f . k
+        Send remote v -> Send remote $ fmap f v
 
 instance Applicative (Task mod d) where
-    pure = Task . pure . Right
+    pure = Task . pure . Pure
     -- TODO better impl (parallel calls)?
     f <*> m = f >>= \f' -> m >>= \a -> pure (f' a)
 
 instance Monad (Task mod d) where
-    (Task m) >>= f = Task
-        ( m >>= \case
-             Left (Call remote k) -> pure $ Left $ Call remote (k >=> f)
-             Right v           -> runTask $ f v
+    (Task t) >>= f = Task
+        ( t >>= \case
+            Pure v        -> runTask $ f v
+            Call remote k -> pure $ Call remote $ k >=> f
+            Send remote m -> pure $ Send remote $ m >>= f
         )
 
 instance MonadIO (Task mod d) where
-    liftIO = Task . fmap Right
+    liftIO = Task . fmap Pure
 
 call :: (Module mod', d' + 1 <= d) => Task mod' d' a -> Task mod d a
-call t = Task $ pure $ Left $ Call t pure
+call t = Task $ pure $ Call t pure
+
+send :: Module mod' => Task mod' d' () -> Task mod d ()
+send t = Task $ pure $ Send t $ pure ()
 
 data ResponseChain a b where
     NoResponse   :: ResponseChain () b
@@ -96,33 +103,37 @@ executor :: Module mod => Int -> Proxy mod -> IO ()
 executor i (_ :: Proxy mod) = forever $ do
     threadDelay 1000000
     dbg ("loop " ++ show i)
-    msg <- recv @mod
+    msg <- mrecv @mod
     process i msg
 
 process :: Module mod => Int -> Msg mod -> IO ()
 process i (Msg (Task t) resp) = do
     dbg $ "recvd " ++ show i ++ ", resp len: " ++ show (lenResp resp)
     t >>= \case
-        Left (Call remote k) -> do
-            dbg $ "call " ++ show i
-            send $ Msg remote $ WithResponse k resp
-            dbg $ "call sent " ++ show i
-        Right v -> dbg "val" >> case resp of
-            NoResponse           ->
-                dbg ("no resp " ++ show i)
+        Pure v -> dbg "Pure" >> case resp of
+            NoResponse ->
+                dbg ("no response " ++ show i)
             WithResponse k resp' -> do
-                dbg ("resp" ++ show i)
-                send $ Msg (k v) resp'
+                dbg ("response " ++ show i)
+                msend $ Msg (k v) resp'
+        Call remote k -> do
+            dbg $ "call " ++ show i
+            msend $ Msg remote $ WithResponse k resp
+            dbg $ "called" ++ show i
+        Send remote m -> do
+            dbg $ "send " ++ show i
+            msend $ Msg remote NoResponse
+            dbg $ "sent remote " ++ show i
+            msend $ Msg m resp
+            dbg $ "sent to myself " ++ show i
 
-send :: Module mod => Msg mod -> IO ()
-send msg = do
+msend :: Module mod => Msg mod -> IO ()
+msend msg = do
     let (Mailbox tq) = mailbox
-    dbg "sending"
     atomically $ modifyTVar tq (push msg)
-    dbg "sent"
 
-recv :: Module mod => IO (Msg mod)
-recv = atomically $ do
+mrecv :: Module mod => IO (Msg mod)
+mrecv = atomically $ do
     let (Mailbox tq) = mailbox
     q <- readTVar tq
     case pop q of
@@ -145,25 +156,21 @@ instance Module M1 where
 instance Module M2 where
     mailbox = m2box
 
--- TODO: f1 + g1 = memory leak (infinite response chain)
--- limit length of response chain
--- introduce CallWithoutResponse?
--- or somehow make calling 'Task mod ()' automatically a call without response
---test1 :: Task M1 ()
---test1 = call $ f1 0
---
---f1 :: Int -> Task M2 ()
---f1 x = do
---    liftIO $ putStrLn $ "f1 " ++ show x
---    call $ g1 (x + 1)
---
---g1 :: Int -> Task M1 ()
---g1 x = do
---    liftIO $ putStrLn $ "g1 " ++ show x
---    call $ f1 (x + 1)
+test1 :: Task M1 0 ()
+test1 = send $ f1 0
 
-test2 :: Task M2 2 ()
-test2 = liftIO (newIORef 0) >>= call . f2
+f1 :: Int -> Task M2 0 ()
+f1 x = do
+    liftIO $ putStrLn $ "f1 " ++ show x
+    send $ g1 (x + 1)
+
+g1 :: Int -> Task M1 0 ()
+g1 x = do
+    liftIO $ putStrLn $ "g1 " ++ show x
+    send $ f1 (x + 1)
+
+test2 :: Task M2 1 ()
+test2 = liftIO (newIORef 0) >>= send . f2
 
 f2 :: IORef Int -> Task M1 1 ()
 f2 st = forever $ do
@@ -186,7 +193,7 @@ main = do
     _ <- case m2box of
         Mailbox b -> b `seq` pure ()
 
-    --send $ Msg test1 NoResponse
-    send $ Msg test2 NoResponse
+    msend $ Msg test1 NoResponse
+    msend $ Msg test2 NoResponse
     _ <- forkIO $ executor 1 (Proxy :: Proxy M1)
     executor 2 (Proxy :: Proxy M2)
