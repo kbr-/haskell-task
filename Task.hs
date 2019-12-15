9c28bbed085777568eca53c7a36a34a31c06a71d
Kamil Braun <kb346840@students.mimuw.edu.pl>
2019-12-15 14:53:13 +0100

    limit call depth

--- Task.hs
+++ Task.hs
@@ -3,6 +3,9 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
 
 module Main where
 
@@ -13,6 +16,8 @@ import Control.Monad.IO.Class
 import Data.IORef
 import Control.Concurrent
 import Control.Concurrent.STM
+
+import GHC.TypeLits
 
 import System.IO.Unsafe
 
@@ -35,44 +40,41 @@ pop q@Queue{..} = case front of
 emptyQ :: Queue a
 emptyQ = Queue { front = [], back = [] }
 
-newtype Task mod a = Task
-    { runTask :: IO (Either (Call mod a) a) }
+newtype Task mod (d :: Nat) a = Task
+    { runTask :: IO (Either (Call mod d a) a) }
 
-data Call mod a where
-    Call :: Module mod' => Task mod' b -> (b -> Task mod a) -> Call mod a
+data Call mod (d :: Nat) a where
+    Call :: (Module mod', d' + 1 <= d) => Task mod' d' b -> (b -> Task mod d a) -> Call mod d a
 
-instance Functor (Task mod) where
+instance Functor (Task mod d) where
     fmap f (Task m) = Task $ flip fmap m $ \case
         Left (Call remote k) -> Left  $ Call remote $ fmap f . k
         Right v           -> Right $ f v
 
-instance Applicative (Task mod) where
+instance Applicative (Task mod d) where
     pure = Task . pure . Right
     -- TODO better impl (parallel calls)?
     f <*> m = f >>= \f' -> m >>= \a -> pure (f' a)
 
-instance Monad (Task mod) where
-    return  = Task . pure . Right
+instance Monad (Task mod d) where
     (Task m) >>= f = Task
         ( m >>= \case
              Left (Call remote k) -> pure $ Left $ Call remote (k >=> f)
              Right v           -> runTask $ f v
         )
 
-instance MonadIO (Task mod) where
+instance MonadIO (Task mod d) where
     liftIO = Task . fmap Right
 
-call :: Module mod' => Task mod' a -> Task mod a
+call :: (Module mod', d' + 1 <= d) => Task mod' d' a -> Task mod d a
 call t = Task $ pure $ Left $ Call t pure
 
--- TODO: limit length of response chain
--- can we do it in the type of Task somehow?
 data ResponseChain a b where
     NoResponse   :: ResponseChain () b
-    WithResponse :: Module mod => (a -> Task mod c) -> ResponseChain c b -> ResponseChain a c
+    WithResponse :: Module mod => (a -> Task mod d c) -> ResponseChain c b -> ResponseChain a c
 
 data Msg mod where
-    Msg :: Task mod a -> ResponseChain a b -> Msg mod
+    Msg :: Task mod d a -> ResponseChain a b -> Msg mod
 
 newtype Mailbox mod
     = Mailbox { mail :: TVar (Queue (Msg mod)) }
@@ -147,23 +149,23 @@ instance Module M2 where
 -- limit length of response chain
 -- introduce CallWithoutResponse?
 -- or somehow make calling 'Task mod ()' automatically a call without response
-test1 :: Task M1 ()
-test1 = call $ f1 0
+--test1 :: Task M1 ()
+--test1 = call $ f1 0
+--
+--f1 :: Int -> Task M2 ()
+--f1 x = do
+--    liftIO $ putStrLn $ "f1 " ++ show x
+--    call $ g1 (x + 1)
+--
+--g1 :: Int -> Task M1 ()
+--g1 x = do
+--    liftIO $ putStrLn $ "g1 " ++ show x
+--    call $ f1 (x + 1)
 
-f1 :: Int -> Task M2 ()
-f1 x = do
-    liftIO $ putStrLn $ "f1 " ++ show x
-    call $ g1 (x + 1)
-
-g1 :: Int -> Task M1 ()
-g1 x = do
-    liftIO $ putStrLn $ "g1 " ++ show x
-    call $ f1 (x + 1)
-
-test2 :: Task M2 ()
+test2 :: Task M2 2 ()
 test2 = liftIO (newIORef 0) >>= call . f2
 
-f2 :: IORef Int -> Task M1 ()
+f2 :: IORef Int -> Task M1 1 ()
 f2 st = forever $ do
     x <- liftIO $ readIORef st
     liftIO $ putStrLn $ "f2: x = " ++ show x
@@ -171,7 +173,7 @@ f2 st = forever $ do
     liftIO $ putStrLn $ "f2: g2(x) returned " ++ show y
     liftIO $ writeIORef st y
 
-g2 :: Int -> Task M2 Int
+g2 :: Int -> Task M2 0 Int
 g2 x = do
     liftIO $ putStrLn $ "g2: x = " ++ show x
     pure (x + 1)
@@ -184,7 +186,7 @@ main = do
     _ <- case m2box of
         Mailbox b -> b `seq` pure ()
 
-    send $ Msg test1 NoResponse
+    --send $ Msg test1 NoResponse
     send $ Msg test2 NoResponse
     _ <- forkIO $ executor 1 (Proxy :: Proxy M1)
     executor 2 (Proxy :: Proxy M2)
